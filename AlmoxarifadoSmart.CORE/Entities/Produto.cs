4c5f9e1d60d66e421c5ed1fbad9cdc184243e70c
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-08 22:14:30 -0300

    Adicionar arquivos de projeto.

---
+++ AlmoxarifadoSmart.CORE/Entities/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlmoxarifadoSmart.API
+{
+    public partial class Produto
+    {
+        public Produto()
+        {
+            IdRequisicaos = new HashSet<Requisicao>();
+        }
+
+        public int Id { get; set; }
+        public string Descricao { get; set; } = null!;
+        public decimal Preco { get; set; }
+        public int? EstoqueAtual { get; set; }
+        public int EstoqueMinimo { get; set; }
+
+        public virtual ICollection<Requisicao> IdRequisicaos { get; set; }
+    }
+}
